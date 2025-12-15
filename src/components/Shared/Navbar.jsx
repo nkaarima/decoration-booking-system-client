1bd2ee548c5196cab5db912140da6ff2ec72dfdd
Kazi Kaarima Nashia <nashia.kazi@gmail.com>
2025-12-15 13:26:38 +0600

    navbar

---
+++ src/components/Shared/Navbar.jsx
@@ -0,0 +1,104 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import React, { useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { Link, NavLink } from 'react-router';
+import logoImg from '../../assets/logo.jpg'
+
+const Navbar = () => {
+
+    const [isOpen,setIsOpen] = useState(false);
+
+    const links = 
+    <>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/service">Services</NavLink></li>
+      <li><NavLink to="/about-us">About Us</NavLink></li>
+      <li><NavLink to="/contact">Contact</NavLink></li>
+    </>
+
+    
+    return (
+
+            <div className="navbar bg-base-100 shadow-sm">
+                
+                <div className="navbar-start">
+                    <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm  dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        {links}
+                    </ul>
+                    </div>
+                     
+                     <div className="flex items-center">
+                        
+                     <img src={logoImg} className="w-1/4 h-1/4" alt="" />
+                    <a className="btn btn-ghost text-3xl font-bold">Smart Decor</a>
+                     </div>
+                </div>
+
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                    {links}
+                    </ul>
+                </div>
+
+                <div className="navbar-end">
+                  
+                   {/* Dropdown Menu */}
+                  
+                    <div className='relative'>
+                    
+                    <div className='flex flex-row items-center gap-3'>
+                        {/* Dropdown btn */}
+                        <div
+                                onClick={() => setIsOpen(!isOpen)}
+                                className='p-4 md:py-1 md:px-2 border border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
+                                >
+                                <AiOutlineMenu />
+                                <div className='hidden md:block'>
+                                    {/* Avatar */}
+                                    <img
+                                    className='rounded-full'
+                                    referrerPolicy='no-referrer'
+                                    alt='profile'
+                                    height='30'
+                                    width='30'
+                                    />
+                                </div>
+
+                         </div>
+                    </div>
+
+                    {isOpen && (
+                        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm'>
+                        <div className='flex flex-col cursor-pointer'>
+                            <Link
+                            to='/'
+                            className='block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                            >
+                            Home
+                            </Link>
+                            
+                            <Link className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'>Dashboard</Link>
+                            <Link className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'>SignUp</Link>
+                            
+                        </div>
+                        </div>
+                    )}
+                    </div>
+                  
+                   
+                </div>
+        
+        </div>
+            
+       
+        
+    );
+};
+
+export default Navbar;
